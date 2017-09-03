bad032ed5a3107ceb5e53cf6ec33e11a820521d9
Quy <quy.nguyenngoctp@gmail.com>
2017-09-03 16:38:00 +0700

    complete new, create, index action of Purposes Controller

--- app/controllers/purposes_controller.rb
+++ app/controllers/purposes_controller.rb
@@ -1,4 +1,23 @@
 class PurposesController < ApplicationController
   def new
   end
+
+  def create
+    @purpose = Purpose.new(purpose_params)
+    if @purpose.save 
+      flash[:success] = "Created new purpose successfully."
+      render "index"
+    else 
+      flash[:error] = "Fail in create new purpose."
+      render "new"
+    end 
+  end
+
+  def index 
+    @purposes = Purpose.all 
+  end  
+
+  def purpose_params
+    params.require(:purpose).permit(:purpose_name)
+  end 
 end
