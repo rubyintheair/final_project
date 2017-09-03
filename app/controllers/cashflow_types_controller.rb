e502ae5ec57b172bbae61c83ca1a8c1da6a2a285
Quy <quy.nguyenngoctp@gmail.com>
2017-09-03 17:27:32 +0700

    complete general new, create, index of cashflowtypes

--- app/controllers/cashflow_types_controller.rb
+++ app/controllers/cashflow_types_controller.rb
@@ -1,4 +1,23 @@
 class CashflowTypesController < ApplicationController
   def new
   end
+
+  def create 
+    @cashflowtype = CashflowType.new(cashflowtype_params)
+    if @cashflowtype.save 
+      flash[:success] = "Quy created a cashflowtype successfully"
+      render "index"
+    else 
+      flash[:error] = "It's failed to create a cashflowtype"
+      render "new"
+    end 
+  end 
+
+  def index 
+    @cashflowtypes = CashflowType.all 
+  end 
+
+  def cashflowtype_params
+    params.require(:cashflow_type).permit(:trend)
+  end 
 end
