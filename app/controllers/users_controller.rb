646b6b9dd94c965a4727f62ec93790c01ef67645
Quy <quy.nguyenngoctp@gmail.com>
2017-09-03 09:41:46 +0700

    create user_form, homes controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+  def index 
+    @users = User.all
+  end 
+
   def new
-    @user = User.new
   end
+
+  def create 
+    @user = User.new(users_params) 
+    if @user.save 
+      flash[:success] = "User created"
+      render "index"
+    else 
+      flash[:error] = "Can not create user"
+      redirect_back
+    end 
+  end 
+
+  def users_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  root "homes#index"
 end
