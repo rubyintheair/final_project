3c24de3883958fef66c4b860963472cd562840d5
Quy <quy.nguyenngoctp@gmail.com>
2017-09-05 00:17:52 +0700

    update select_tag of form for @daily_cash_flow

--- app/controllers/daily_cashflows_controller.rb
+++ app/controllers/daily_cashflows_controller.rb
@@ -1,6 +1,6 @@
 class DailyCashflowsController < ApplicationController
   def new
-    @daily_cash_flow = current_user.daily_cashflows.new
+    @daily_cash_flow = current_user.daily_cashflows.build
     @cashflow_types = CashflowType.all
     @friends = Friend.all 
     @purposes = Purpose.all 
