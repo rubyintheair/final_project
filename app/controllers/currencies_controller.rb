ca8d40087202f7d6fd8b5fa750a4ece522f549bb
Quy <quy.nguyenngoctp@gmail.com>
2017-09-05 21:04:45 +0700

    generate new, create, index method of Currency Controller

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,6 +2,21 @@ class CurrenciesController < ApplicationController
   def new
   end
 
+  def create 
+    @currency = Currency.new(currency_params)
+    if @currency.save 
+      flash[:success] = "Quy da tao Currency thanh cong"
+      render "index"
+    else 
+      flash[:error] = "Quy khong the tao Currency roi #{@currency.errors.full_messages.to_sentence}"
+      render "new"
+    end 
+  end 
+
   def index
   end
+
+  def currency_params
+    params.require(:currency).permit(:name)
+  end 
 end
