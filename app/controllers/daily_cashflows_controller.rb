0cc79134d8522f574c9a2fb997ad2d657af87a79
Quy <quy.nguyenngoctp@gmail.com>
2017-09-05 18:06:04 +0700

    complete form_for daily_cashflow

--- app/controllers/daily_cashflows_controller.rb
+++ app/controllers/daily_cashflows_controller.rb
@@ -1,11 +1,29 @@
 class DailyCashflowsController < ApplicationController
   def new
     @daily_cash_flow = current_user.daily_cashflows.build
-    @cashflow_types = CashflowType.all
-    @friends = Friend.all 
-    @purposes = Purpose.all 
   end
 
+  def create 
+    @daily_cash_flow = current_user.daily_cashflows.build(daily_cashflow_params)
+    @daily_cash_flow.cashflow_type = CashflowType.find(params[:cashflow_types_id].to_i)
+    @daily_cash_flow.purpose = Purpose.find(params[:purposes_id].to_i)
+    if @daily_cash_flow.save 
+      if Friend.find(params[:friends_id].to_i)
+        @daily_cash_flow.friend = Friend.find(params[:friends_id].to_i)
+      end 
+      flash[:success] = "Create Daily Cashflow successfully"
+      render "index"
+    else 
+      flash[:error] = "Fail to create a daily Cashflow #{@daily_cash_flow.errors.full_messages.to_sentence}"
+      render "new"
+    end 
+  end 
+
+  def daily_cashflow_params
+    params.require(:daily_cashflow).permit(:amount, :occur_at, :content)
+  end 
+
   def index
+    @daily_cash_flows = DailyCashflow.all
   end
 end
--- app/models/daily_cashflow.rb
+++ app/models/daily_cashflow.rb
@@ -1,6 +1,6 @@
 class DailyCashflow < ApplicationRecord
   belongs_to :user
-  belongs_to :friend
+  belongs_to :friend, optional: :true
   belongs_to :purpose
   belongs_to :cashflow_type
   validates :amount, :occur_at, :content, presence: true
