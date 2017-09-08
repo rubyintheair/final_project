c5a4674e4b94db309d4484c395a669b30114dc4c
Quy <quy.nguyenngoctp@gmail.com>
2017-09-08 09:59:55 +0700

    update controller_daily_cashflows action: search

--- app/controllers/daily_cashflows_controller.rb
+++ app/controllers/daily_cashflows_controller.rb
@@ -54,21 +54,24 @@ class DailyCashflowsController < ApplicationController
   end 
 
   def search
+    @cashflows = current_user.daily_cashflows
     if (params[:purposes_id] == "14")
       if (params[:start_date] && params[:end_date])
-        @period_cashflows = DailyCashflow.where("date(occur_at) in (?)", params[:start_date])
+        @period_cashflows = @cashflows.select {|e| e.occur_at.to_date == params[:start_date]}
+        # @period_cashflows = @cashflows.select {|e| e.occur_at.to_date >= params[:start_date]
+        #                     && e.occur_at.to_date <= params[:end_date]}
         flash[:success] = "We have searched based on START_DATE and END_DATE"
       else 
         flash[:error] = "Nothing in the params"
       end
     else 
       if (params[:start_date] && params[:end_date])
-        @period_cashflows = DailyCashflow.where("date(occur_at) in (?)", 
-                            params[:start_date]).select {|e| e.purpose_id == params[:purposes_id].to_i}
+        @period_cashflows = @cashflows.select {|e| e.occur_at.to_date == params[:start_date]}.select 
+                            {|e| e.purpose_id == params[:purposes_id].to_i}
         flash[:success] = "We have searched based on START_DATE, END_DATE and PURPOSE"
       elsif (params[:purposes_id].to_i) 
         flash[:success] = "We have searched based on PURPOSE"
-        @period_cashflows = DailyCashflow.where("purpose_id": params[:purposes_id].to_i)  
+        @period_cashflows = @cashflows.select {|e| e.purpose_id == params[:purposes_id].to_i } 
       end 
     end 
     
