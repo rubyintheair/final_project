78d6f9c97ddd9211493acabcbf682041f208253d
Quy <quy.nguyenngoctp@gmail.com>
2017-09-21 11:32:31 +0700

    change new icon

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -30,7 +30,7 @@ class Purpose < ApplicationRecord
     when "Clothing" then "http://www.myiconfinder.com/uploads/iconsets/9b11c4309031908f3c2f979686b04e16.png"
     when /Medical/ then "http://www.playamedical.com/images/2013/04/medical-icon-png-33.png"
     when /Savings/ then "https://icon-icons.com/icons2/516/PNG/512/coin_money_icon-icons.com_51091.png"
-    when "Entertainment" then "https://image.flaticon.com/icons/svg/257/257445.svg"
+    when "Entertainment" then "https://cdn3.iconfinder.com/data/icons/black-easy/512/538309-game_512x512.png"
     when "Beauty" then "http://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.6/assets/svg/1f484.svg"
     when "Travel" then "https://cdn4.iconfinder.com/data/icons/travel-gang/512/Travel_23-512.png"
     when "Education" then "https://image.flaticon.com/icons/svg/201/201614.svg"
